a8b4838a0b66c8a4d13cf573a74a4e53135e559c
JERRELL RUSS <jruss1@cnm.edu>
2023-03-20 15:22:51 -0700

    Add project files.

---
+++ InheritanceDemo/HRManager.cs
@@ -0,0 +1,84 @@
+﻿using static InheritanceDemo.Employee;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace InheritanceDemo
+{
+    internal class HRManager
+    {
+
+        static void Main(string[] args)
+        {
+
+            Employee emp = new Employee();
+
+            emp.EmpNum = 1;
+
+            emp.FirstName = "Steve";
+
+            emp.LastName = "Jobs";
+
+
+            HourlyEmployee hourEmp = new HourlyEmployee();
+
+            hourEmp.EmpNum = 2;
+
+            hourEmp.FirstName = "Bill";
+
+            hourEmp.LastName = "Gates";
+
+            hourEmp.HourlyRate = 15.00M;
+            Console.WriteLine("emp.GetPaySummary: " + emp.GetPaySummary());
+
+            Console.WriteLine("hourEmp.GetPaySummary: " + hourEmp.GetPaySummary());
+
+
+            Employee emp2 = hourEmp;
+
+            Console.WriteLine(emp.PaySummary());
+            Console.WriteLine(hourEmp.PaySummary());
+            Console.WriteLine(emp2.PaySummary());
+
+
+
+
+        }
+         
+        //public class HourlyEmployee : Employee
+        //{
+
+        //    public decimal HourlyRate { get; set; }
+
+        //    public HourlyEmployee() : base()
+        //    {
+
+        //        //HourlyRate = 15.0M;
+        //        HourlyEmployee hourEmp = new HourlyEmployee();
+
+        //        hourEmp.EmpNum = 2;
+
+        //        hourEmp.FirstName = "Bill";
+
+        //        hourEmp.LastName = "Gates";
+
+        //        hourEmp.HourlyRate = 15.00M;
+
+        //        Console.WriteLine("hourEmp.GetPaySummary: " + hourEmp.GetPaySummary());
+
+
+        //    }
+
+
+
+
+        //}
+
+
+    }
+
+}
+
+
+
+
+
+
---
+++ InheritanceDemo/HourlyEmployee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static InheritanceDemo.Employee;
+
+namespace InheritanceDemo
+{
+   public class HourlyEmployee:Employee
+   {
+        
+        public decimal HourlyRate { get; set; }
+
+        public HourlyEmployee() {
+            HourlyRate = 30.00M;
+        
+        }
+        
+        
+        public string GetPaySummary()
+        {
+            return "This employee is payed " + HourlyRate + " per hour";
+        }
+
+
+        public string PaySummary()
+        {
+            get { return $"This employee is payed {HourlyRate} per hour"; }
+
+
+        }
+
+
+   }
+}
